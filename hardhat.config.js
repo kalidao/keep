ec8f15b61f3fa136ddb22fb6aa718e8b3ec9dd1a
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-02-28 20:17:46 -0700

    Create hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,83 @@
+require("@nomiclabs/hardhat-waffle");
+require("hardhat-gas-reporter");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  defaultNetwork: "hardhat",
+  gasReporter: {
+    currency: "USD",
+    enabled: true,
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    alice: {
+      default: 1,
+    },
+    bob: {
+      default: 2,
+    },
+    carol: {
+      default: 3,
+    },
+  },
+  networks: {
+    localhost: {
+      live: false,
+      saveDeployments: true,
+      tags: ["local"],
+    },
+    hardhat: {
+      forking: {
+        enabled: true,
+        url: "https://eth-mainnet.alchemyapi.io/v2/zjx8baDblg7pbUjvc4zuRARLT28Ft2PC",
+        blockNumber: 12886725
+      },
+      allowUnlimitedContractSize: true,
+      live: false,
+      saveDeployments: true,
+      tags: ["test", "local"],
+    },
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.12",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 999999,
+          },
+        },
+      },
+      {
+        version: "0.6.12",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 999999,
+          },
+        },
+      },
+    ],
+  },
+  mocha: {
+    timeout: 200000
+  },
+};
