15375448571c7c92ab5d1e0d9987e56f32a77de0
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-03-06 15:51:51 -0500

    Update Multicall.sol
    🥢lil optimizations

--- src/utils/Multicall.sol
+++ src/utils/Multicall.sol
@@ -9,22 +9,24 @@ abstract contract Multicall {
     function multicall(bytes[] calldata data) public payable returns (bytes[] memory results) {
         results = new bytes[](data.length);
         
-        // cannot realistically overflow on human timescales
-        unchecked {
-            for (uint256 i = 0; i < data.length; ++i) {
-                (bool success, bytes memory result) = address(this).delegatecall(data[i]);
+        for (uint256 i; i < data.length;) {
+            (bool success, bytes memory result) = address(this).delegatecall(data[i]);
 
-                if (!success) {
-                    if (result.length < 68) revert();
+            if (!success) {
+                if (result.length < 68) revert();
                     
-                    assembly {
-                        result := add(result, 0x04)
-                    }
-                    
-                    revert(abi.decode(result, (string)));
+                assembly {
+                    result := add(result, 0x04)
                 }
+                    
+                revert(abi.decode(result, (string)));
+            }
 
-                results[i] = result;
+            results[i] = result;
+
+            // cannot realistically overflow on human timescales
+            unchecked {
+                ++i;
             }
         }
     }
