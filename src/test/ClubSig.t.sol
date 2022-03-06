a3fa42c9876ad23772d5f6b18c73848ca55d5769
Andreas Bigger <abigger87@gmail.com>
2022-03-05 18:13:46 -0800

    :gear: build tests :test_tube:

--- src/test/ClubSig.t.sol
+++ src/test/ClubSig.t.sol
@@ -1,19 +1,43 @@
 // SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity 0.8.12;
+pragma solidity >=0.8.4;
 
+import {ClubSig} from "../ClubSig.sol";
 import {DSTestPlus} from "./utils/DSTestPlus.sol";
 
-import {Greeter} from "../Greeter.sol";
+contract ClubSigTest is DSTestPlus {
+    ClubSig clubSig;
 
-contract GreeterTest is DSTestPlus {
-    Greeter greeter;
+    /// @dev Users
+    address public alice = address(0xa);
+    address public bob = address(0xb);
 
+    /// @notice Set up the testing suite
     function setUp() public {
-        greeter = new Greeter("gm");
+      clubSig = new ClubSig();
+
+      // Create the Club[]
+      ClubSig.Club[] memory clubs = new ClubSig.Club[](2);
+      clubs[0] = ClubSig.Club(alice, 0, 100);
+      clubs[1] = ClubSig.Club(bob, 1, 100);
+
+      // Initialize
+      clubSig.init(
+        clubs,
+        2,
+        false,
+        "BASE"
+      );
+
+      // Sanity check initialization
+      assertEq(keccak256(bytes(clubSig.baseURI())), keccak256(bytes("BASE")));
     }
 
-    function testSetGm() public {
-        greeter.setGm("gm gm");
-        greeter.gm("gm gm");
+    /// -----------------------------------------------------------------------
+    /// Operations
+    /// -----------------------------------------------------------------------
+
+    function testFlipGovernor(address charlie) public {
+      vm.expectRevert(bytes4(keccak256("Forbidden()")));
+      clubSig.flipGovernor(charlie);
     }
 }
