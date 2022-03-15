7a186ccd3d2a19d1c2c7200fccadc232ad8f99ab
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-03-15 19:03:49 -0400

    Update ClubSig.t.sol
    simplify

--- src/test/ClubSig.t.sol
+++ src/test/ClubSig.t.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity >=0.8.4;
 
-import {IClub} from "../interfaces/IClub.sol";
 import {ClubSig} from "../ClubSig.sol";
 import {DSTestPlus} from "./utils/DSTestPlus.sol";
 
