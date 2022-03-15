06174f6d05bc8f997eef2b68748d61616f3d58a0
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-03-15 02:49:05 -0400

    Update ClubSig.sol
    make code easier to read

--- src/ClubSig.sol
+++ src/ClubSig.sol
@@ -9,8 +9,9 @@ import {NFTreceiver} from './utils/NFTreceiver.sol';
 import {IERC1271} from './interfaces/IERC1271.sol';
 import {IERC20minimal} from './interfaces/IERC20minimal.sol';
 
-import {Base64} from './libraries/Base64.sol';
+import {FixedPointMathLib} from './libraries/FixedPointMathLib.sol';
 import {SafeTransferTokenLib} from './libraries/SafeTransferTokenLib.sol';
+import {URIbuilder} from './libraries/URIbuilder.sol';
 
 /// @title ClubSig
 /// @notice EIP-712-signed multi-signature contract with ragequit and NFT identifiers for signers
@@ -28,9 +29,10 @@ contract ClubSig is ClubNFT, Multicall {
     /// Events
     /// -----------------------------------------------------------------------
 
-    event Execute(address indexed target, uint256 value, bytes payload);
+    event Execute(address indexed to, uint256 value, bytes data);
     event Govern(Club[] club, bool[] mints, uint256 quorum);
     event GovernorFlipped(address indexed account);
+    event DocsUpdated(string docs);
     event URIupdated(string uri);
 
     /// -----------------------------------------------------------------------
@@ -40,7 +42,6 @@ contract ClubSig is ClubNFT, Multicall {
     error Initialized();
     error SigsBounded();
     error WrongSigner();
-    error ExecuteFailed();
     error NoArrayParity();
     error AssetOrder();
 
@@ -52,7 +53,9 @@ contract ClubSig is ClubNFT, Multicall {
     uint256 public nonce;
     /// @dev signature (NFT) threshold to execute tx
     uint256 public quorum;
-    /// @dev optional metadata to signify contract
+    /// @dev metadata signifying club agreements
+    string public docs;
+    /// @dev optional metadata signifying club logo
     string public baseURI;
     /// @dev total signer units minted
     uint256 public totalSupply;
@@ -60,8 +63,6 @@ contract ClubSig is ClubNFT, Multicall {
     uint256 public totalLoot;
     /// @dev ragequittable units per account
     mapping(address => uint256) public loot;
-    /// @dev last daily allowance claim
-    mapping(address => uint256) public lastClaim;
     /// @dev administrative account tracking
     mapping(address => bool) public governor;
 
@@ -69,14 +70,6 @@ contract ClubSig is ClubNFT, Multicall {
         address signer;
         uint256 id;
         uint256 loot;
-    }
-
-    /// @dev Pack address+bool into one slot
-    struct Call {
-        address target;
-        bool call;
-        uint256 value;
-        bytes payload;
     }
 
     /// -----------------------------------------------------------------------
@@ -117,7 +110,8 @@ contract ClubSig is ClubNFT, Multicall {
         Club[] calldata club_,
         uint256 quorum_,
         bool paused_,
-        string memory baseURI_
+        string calldata docs_,
+        string calldata baseURI_
     ) external payable {
         if (nonce != 0) revert Initialized();
 
@@ -153,6 +147,7 @@ contract ClubSig is ClubNFT, Multicall {
 
         nonce = 1;
         quorum = quorum_;
+        docs = docs_;
         baseURI = baseURI_;
 
         INITIAL_CHAIN_ID = block.chainid;
@@ -165,108 +160,37 @@ contract ClubSig is ClubNFT, Multicall {
 
     function tokenURI(uint256 id) external view returns (string memory) {
         bytes memory base = bytes(baseURI);
-        return base.length != 0 ? baseURI : _buildTokenURI(id);
-    }
 
-    function _buildTokenURI(uint256 id) internal view returns (string memory) {
-        address owner = ownerOf[id];
-
-        string memory metaSVG = string(
-            abi.encodePacked(
-                '<text dominant-baseline="middle" text-anchor="middle" fill="white" x="50%" y="90px">',
-                '0x',
-                _addressToString(owner),
-                '</text>',
-                '<text dominant-baseline="middle" text-anchor="middle" fill="white" x="100%" y="180px">',
-                _uintToString(loot[owner]),
-                ' Loot',
-                '</text>'
-            )
-        );
-        bytes memory svg = abi.encodePacked(
-            '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 400" preserveAspectRatio="xMidYMid meet" style="font:14px serif"><rect width="400" height="400" fill="black" />',
-            metaSVG,
-            '</svg>'
-        );
-        bytes memory image = abi.encodePacked(
-            'data:image/svg+xml;base64,',
-            Base64.encode(bytes(svg))
-        );
-        return string(
-            abi.encodePacked(
-                'data:application/json;base64,',
-                Base64.encode(
-                    bytes(
-                        abi.encodePacked(
-                            '{"name":"',
-                            name(),
-                            '", "image":"',
-                            image,
-                            '", "description": "The holder of this NFT is a club key signer with impeccable taste."}'
-                        )
-                    )
-                )
-            )
-        );
-    }
-
-    function _addressToString(address addr) internal pure returns (string memory) {
-        bytes memory s = new bytes(40);
-        for (uint256 i; i < 20;) {
-            bytes1 b = bytes1(uint8(uint256(uint160(addr)) / (2**(8*(19 - i)))));
-            bytes1 hi = bytes1(uint8(b) / 16);
-            bytes1 lo = bytes1(uint8(b) - 16 * uint8(hi));
-            s[2*i] = _char(hi);
-            s[2*i+1] = _char(lo);
-            // cannot realistically overflow on human timescales
-            unchecked { 
-                ++i; 
-            }
+        if (base.length == 0) {
+            address owner = ownerOf[id];
+            return URIbuilder._buildTokenURI(owner, loot[owner], name());
+        } else {
+            return baseURI;
         }
-        return string(s);
-    }
-
-    function _char(bytes1 b) internal pure returns (bytes1 c) {
-        if (uint8(b) < 10) return bytes1(uint8(b) + 0x30);
-        else return bytes1(uint8(b) + 0x57);
-    }
-
-    function _uintToString(uint256 value) internal pure returns (string memory) {
-        if (value == 0) return '0';
-        uint256 temp = value;
-        uint256 digits;
-        while (temp != 0) {
-            // cannot realistically overflow on human timescales
-            unchecked {
-                ++digits;
-            }
-            temp /= 10;
-        }
-        bytes memory buffer = new bytes(digits);
-        while (value != 0) {
-            digits -= 1;
-            buffer[digits] = bytes1(uint8(48 + uint256(value % 10)));
-            value /= 10;
-        }
-        return string(buffer);
     }
 
     /// -----------------------------------------------------------------------
     /// Operations
     /// -----------------------------------------------------------------------
 
-    function execute(Call calldata call, Signature[] calldata sigs) external payable returns (bool success, bytes memory result) {
+    function execute(
+        address to, 
+        uint256 value, 
+        bytes memory data, 
+        bool deleg, 
+        Signature[] calldata sigs
+    ) external payable returns (bool success) {
         unchecked {
             bytes32 digest = keccak256(abi.encodePacked('\x19\x01', DOMAIN_SEPARATOR(),
                 keccak256(abi.encode(keccak256(
-                    'Exec(address target,uint256 value,bytes payload,bool call,uint256 nonce)'),
+                    'Exec(address to,uint256 value,bytes data,bool deleg,uint256 nonce)'),
                     // cannot realistically overflow on human timescales
-                    call.target, call.value, call.payload, call.call, ++nonce)))
+                    to, value, data, deleg, ++nonce)))
                 );
 
             address prevAddr;
 
-            for (uint256 i; i < quorum;) {
+            for (uint256 i; i < quorum; ++i) {
                 address signer = ecrecover(digest, sigs[i].v, sigs[i].r, sigs[i].s);
                 // check for conformant contract signature
                 if (signer.code.length != 0 && IERC1271(signer).isValidSignature(
@@ -275,20 +199,22 @@ contract ClubSig is ClubNFT, Multicall {
                 // check for NFT balance and duplicates
                 if (balanceOf[signer] == 0 || prevAddr >= signer) revert WrongSigner();
                 prevAddr = signer;
-
-                ++i;
             }
         }
 
-        if (call.call) {
-            (success, result) = call.target.call{value: call.value}(call.payload);
-            if (!success) revert ExecuteFailed();
+        uint256 gasLeft = gasleft() - 2500;
+
+        if (!deleg) {
+            assembly {
+                success := call(gasLeft, to, value, add(data, 0x20), mload(data), 0, 0)
+            }
         } else { // delegate call
-            (success, result) = call.target.delegatecall(call.payload);
-            if (!success) revert ExecuteFailed();
+            assembly {
+                success := delegatecall(gasLeft, to, add(data, 0x20), mload(data), 0, 0)
+            }
         }
 
-        emit Execute(call.target, call.value, call.payload);
+        emit Execute(to, value, data);
     }
 
     function govern(
@@ -344,37 +270,49 @@ contract ClubSig is ClubNFT, Multicall {
         emit Govern(club_, mints_, quorum_);
     }
 
-    function governorExecute(Call calldata call) external payable returns (bool success, bytes memory result) {
+    function governorExecute(
+        address to, 
+        uint256 value, 
+        bytes memory data, 
+        bool deleg
+    ) external payable returns (bool success) {
         if (!governor[msg.sender]) revert Forbidden();
 
-        if (call.call) {
-            (success, result) = call.target.call{value: call.value}(call.payload);
-            if (!success) revert ExecuteFailed();
-        } else {
-            (success, result) = call.target.delegatecall(call.payload);
-            if (!success) revert ExecuteFailed();
+        uint256 gasLeft = gasleft() - 2500;
+
+        if (!deleg) {
+            assembly {
+                success := call(gasLeft, to, value, add(data, 0x20), mload(data), 0, 0)
+            }
+        } else { // delegate call
+            assembly {
+                success := delegatecall(gasLeft, to, add(data, 0x20), mload(data), 0, 0)
+            }
         }
+
+        emit Execute(to, value, data);
     }
 
     function flipGovernor(address account) external payable {
         if (msg.sender != address(this) && !governor[msg.sender]) revert Forbidden();
-
         governor[account] = !governor[account];
-
         emit GovernorFlipped(account);
     }
 
     function flipPause() external payable {
         if (msg.sender != address(this) && !governor[msg.sender]) revert Forbidden();
-
         ClubNFT._flipPause();
+    }
+
+    function updateDocs(string calldata docs_) external payable {
+        if (msg.sender != address(this) && !governor[msg.sender]) revert Forbidden();
+        docs = docs_;
+        emit DocsUpdated(docs_);
     }
 
     function updateURI(string calldata baseURI_) external payable {
         if (msg.sender != address(this) && !governor[msg.sender]) revert Forbidden();
-
         baseURI = baseURI_;
-
         emit URIupdated(baseURI_);
     }
 
@@ -398,9 +336,14 @@ contract ClubSig is ClubNFT, Multicall {
             // prevent null and duplicate assets
             if (prevAddr >= assets[i]) revert AssetOrder();
             prevAddr = assets[i];
+
             // calculate fair share of given assets for redemption
-            uint256 amountToRedeem = lootToBurn * IERC20minimal(assets[i]).balanceOf(address(this)) / 
-                lootTotal;
+            uint256 amountToRedeem = FixedPointMathLib.mulDivDown(
+                lootToBurn, 
+                IERC20minimal(assets[i]).balanceOf(address(this)), 
+                lootTotal
+            );
+            
             // transfer to redeemer
             if (amountToRedeem != 0) assets[i]._safeTransfer(msg.sender, amountToRedeem);
             // cannot realistically overflow on human timescales
