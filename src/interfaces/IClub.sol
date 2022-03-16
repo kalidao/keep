ee40ade48701ec40bb475d9793bb468cd22a68a9
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-03-16 19:07:50 -0400

    Update IClub.sol
    nit

--- src/interfaces/IClub.sol
+++ src/interfaces/IClub.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity >=0.8.4;
 
-/// @notice ClubSig membership interface
+/// @notice Kali ClubSig membership interface
 interface IClub {
     struct Club {
         address signer;
