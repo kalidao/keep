4ea355fd45083ab60db75b90a5e7065e2d0f76b1
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-03-16 19:08:06 -0400

    Update IClubLoot.sol
    nit

--- src/interfaces/IClubLoot.sol
+++ src/interfaces/IClubLoot.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity >=0.8.4;
 
-/// @notice Minimal ERC-20 interface with ClubSig extensions
+/// @notice Minimal ERC-20 interface with Kali ClubSig extensions
 interface IClubLoot { 
     function balanceOf(address account) external view returns (uint256);
     function totalSupply() external view returns (uint256);
