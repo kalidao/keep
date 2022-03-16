c5f2cdf8dc748c26f97c58647a521c221558e84c
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-03-15 22:52:45 -0400

    Update and rename IERC20minimal.sol to IClubToken.sol

--- src/interfaces/IERC20minimal.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity >=0.8.4;
-
-/// @notice Minimal ERC-20 interface
-interface IERC20minimal { 
-    function balanceOf(address account) external view returns (uint256);
-}
