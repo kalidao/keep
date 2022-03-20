8f1f9f1cbf4475e23640245c8087f6ae46c5583c
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-03-19 23:30:21 -0400

    Update IERC1271.sol
    nit header

--- src/interfaces/IERC1271.sol
+++ src/interfaces/IERC1271.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity >=0.8.4;
 
-/// @dev https://eips.ethereum.org/EIPS/eip-1271
 /// @notice ERC-1271 interface
+/// @dev https://eips.ethereum.org/EIPS/eip-1271
 interface IERC1271 {
     function isValidSignature(bytes32 hash, bytes calldata signature)
         external
