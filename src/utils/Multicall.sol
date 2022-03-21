e084943dc3d3b6d06e9f2dda186be577ca37a227
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-03-20 23:24:02 -0400

    Update Multicall.sol
    security advisory on payable https://github.com/Uniswap/v3-periphery/issues/52

--- src/utils/Multicall.sol
+++ src/utils/Multicall.sol
@@ -5,7 +5,8 @@ pragma solidity >=0.8.4;
 /// @author Modified from Uniswap (https://github.com/Uniswap/v3-periphery/blob/main/contracts/base/Multicall.sol)
 /// License-Identifier: GPL-2.0-or-later
 abstract contract Multicall {
-    // TODO(This should not be external wide open, but rather an access restricted function)
+    /// @dev Be aware of potential issues with payable multicall() (https://github.com/Uniswap/v3-periphery/issues/52)
+    ///  - in Kali ClubSig, msg.value is not used for internal accounting, so we can avoid this risk to save gas
     function multicall(bytes[] calldata data)
         external
         payable
