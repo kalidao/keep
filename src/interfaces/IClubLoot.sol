68ba326eb8d4286a2f4571547e889cea98aefa85
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-03-24 18:20:04 -0400

    Update IClubLoot.sol
    rvrt

--- src/interfaces/IClubLoot.sol
+++ src/interfaces/IClubLoot.sol
@@ -10,4 +10,6 @@ interface IClubLoot {
     function mint(address to, uint256 amount) external;
 
     function govBurn(address from, uint256 amount) external;
+
+    function setPause(bool paused) external;
 }
