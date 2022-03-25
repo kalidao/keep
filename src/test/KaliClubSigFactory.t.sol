bc579d89348cc3188498f5e41e9ef8d9426a7751
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-03-24 20:43:39 -0400

    Update KaliClubSigFactory.t.sol
    nit

--- src/test/KaliClubSigFactory.t.sol
+++ src/test/KaliClubSigFactory.t.sol
@@ -1,7 +1,5 @@
 pragma solidity >=0.8.4;
 
-import {IClub} from "../interfaces/IClub.sol";
-
 import {KaliClubSig} from "../KaliClubSig.sol";
 import {ClubLoot} from "../ClubLoot.sol";
 import {KaliClubSigFactory} from "../KaliClubSigFactory.sol";
@@ -33,9 +31,9 @@ contract KaliClubSigFactoryTest is DSTestPlus {
         KaliClubSig depClubSig;
 
         // Create the Club[]
-        IClub.Club[] memory clubs = new IClub.Club[](2);
-        clubs[0] = IClub.Club(alice, 0, 100);
-        clubs[1] = IClub.Club(bob, 1, 100);
+        Club[] memory clubs = new Club[](2);
+        clubs[0] = Club(alice, 0, 100);
+        clubs[1] = Club(bob, 1, 100);
 
         vm.expectEmit(true, true, true, true);
         (depClubSig, ) = factory.deployClubSig(
