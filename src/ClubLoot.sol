c444ed800e2ea580d0bc048248dc0f78b92aab7f
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-03-24 20:27:52 -0400

    Update ClubLoot.sol
    simplify import

--- src/ClubLoot.sol
+++ src/ClubLoot.sol
@@ -1,12 +1,10 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity >=0.8.4;
 
-import {IClub} from "./interfaces/IClub.sol";
-
 /// @notice Modern, minimalist, and gas efficient ERC-20 + EIP-2612 implementation designed for Kali ClubSig
 /// @author Modified from Solmate (https://github.com/Rari-Capital/solmate/blob/main/src/tokens/ERC20.sol)
 /// License-Identifier: AGPL-3.0-only
-contract ClubLoot is IClub {
+contract ClubLoot {
     /// -----------------------------------------------------------------------
     /// Events
     /// -----------------------------------------------------------------------
