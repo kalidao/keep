12ab1e4e9570e1a00be7ef351fb9e94c0c0e4eb5
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-05-02 18:23:35 +0530

    Update IClubLoot.sol
    dev note

--- src/interfaces/IClubLoot.sol
+++ src/interfaces/IClubLoot.sol
@@ -2,6 +2,7 @@
 pragma solidity >=0.8.4;
 
 /// @notice Minimal ERC-20 interface with Kali ClubSig extensions
+/// @dev https://eips.ethereum.org/EIPS/eip-20
 interface IClubLoot {
     function balanceOf(address account) external view returns (uint256);
 
