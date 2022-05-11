69a744e0f456d3054e0faa610900a4411daa8a5e
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-05-11 22:35:36 +0530

    Update KaliClubSig.t.sol
    ''

--- src/test/KaliClubSig.t.sol
+++ src/test/KaliClubSig.t.sol
@@ -2,7 +2,6 @@
 pragma solidity >=0.8.4;
 
 import {IClub} from '../interfaces/IClub.sol';
-import {IRicardianLLC} from '../interfaces/IRicardianLLC.sol';
 
 import {ClubLoot} from '../ClubLoot.sol';
 import {Call, Signature, KaliClubSig} from '../KaliClubSig.sol';
@@ -40,11 +39,6 @@ contract ClubSigTest is Test {
     uint256 immutable nullPk =
         0x8b2ed20f3cc3dd482830910365cfa157e7568b9c3fa53d9edd3febd61086b9be;
     address public immutable nully = 0x0ACDf2aC839B7ff4cd5F16e884B2153E902253f2;
-
-    /// @dev Integrations
-
-    IRicardianLLC public immutable ricardian =
-        IRicardianLLC(0x2017d429Ad722e1cf8df9F1A2504D4711cDedC49);
 
     /// @dev Helpers
 
@@ -111,7 +105,7 @@ contract ClubSigTest is Test {
         mockDai.mint(address(this), 1000000000 * 1e18);
 
         // Create the factory
-        factory = new KaliClubSigFactory(loot, clubSig, ricardian);
+        factory = new KaliClubSigFactory(loot, clubSig);
 
         // Create the calls
         Call[] memory calls = new Call[](0);
