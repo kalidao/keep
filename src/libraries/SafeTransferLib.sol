6c5cf83c36bf7656b8ced6bea9209f07a86a9197
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-05-13 20:37:01 +0530

    Update SafeTransferLib.sol
    v7 solmate

--- src/libraries/SafeTransferLib.sol
+++ src/libraries/SafeTransferLib.sol
@@ -26,29 +26,25 @@ library SafeTransferLib {
         bool success;
 
         assembly {
-            // get a pointer to some free memory
-            let freeMemoryPointer := mload(0x40)
+            // we'll write our calldata to this slot below, but restore it later
+            let memPointer := mload(0x40)
             // write the abi-encoded calldata into memory, beginning with the function selector
-            mstore(
-                freeMemoryPointer,
-                0xa9059cbb00000000000000000000000000000000000000000000000000000000
-            )
-            mstore(add(freeMemoryPointer, 4), to) // append the 'to' argument
-            mstore(add(freeMemoryPointer, 36), amount) // append the 'amount' argument
+            mstore(0, 0xa9059cbb00000000000000000000000000000000000000000000000000000000)
+            mstore(4, to) // Append the "to" argument.
+            mstore(36, amount) // Append the "amount" argument.
 
             success := and(
                 // set success to whether the call reverted, if not we check it either
                 // returned exactly 1 (can't just be non-zero data), or had no return data
-                or(
-                    and(eq(mload(0), 1), gt(returndatasize(), 31)),
-                    iszero(returndatasize())
-                ),
-                // we use 68 because the length of our calldata totals up like so: 4 + 32 * 2
-                // we use 0 and 32 to copy up to 32 bytes of return data into the scratch space
-                // counterintuitively, this call must be positioned second to the or() call in the
-                // surrounding and() call or else returndatasize() will be zero during the computation
-                call(gas(), token, 0, freeMemoryPointer, 68, 0, 32)
+                or(and(eq(mload(0), 1), gt(returndatasize(), 31)), iszero(returndatasize())),
+                // we use 68 because that's the total length of our calldata (4 + 32 * 2)
+                // - counterintuitively, this call() must be positioned after the or() in the
+                // surrounding and() because and() evaluates its arguments from right to left
+                call(gas(), token, 0, 0, 68, 0, 32)
             )
+
+            mstore(0x60, 0) // restore the zero slot to zero
+            mstore(0x40, memPointer) // restore the memPointer
         }
         if (!success) revert TransferFailed();
     }
