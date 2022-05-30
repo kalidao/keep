5d98003ac9db7910edf6f0f2da607f7e14e39597
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-05-30 22:15:58 +0530

    Update SafeTransferLib.sol
    vectorized optimizations by assembloring revert msgs https://github.com/Rari-Capital/solmate/blob/0d19c363a8da5da02a6713112fa7fcb8df410f2f/src/utils/SafeTransferLib.sol

--- src/libraries/SafeTransferLib.sol
+++ src/libraries/SafeTransferLib.sol
@@ -5,17 +5,18 @@ pragma solidity >=0.8.4;
 /// @author Modified from Solmate (https://github.com/Rari-Capital/solmate/blob/main/src/utils/SafeTransferLib.sol)
 /// License-Identifier: AGPL-3.0-only
 library SafeTransferLib {
-    error ETHtransferFailed();
-    error TransferFailed();
-
     function _safeTransferETH(address to, uint256 amount) internal {
-        bool success;
-
         assembly {
             // transfer the ETH and store if it succeeded or not
-            success := call(gas(), to, amount, 0, 0, 0, 0)
+            let success := call(gas(), to, amount, 0, 0, 0, 0)
+
+            if iszero(success) {
+                mstore(0x64, 0x08c379a0) // function selector of the error method, offseted
+                mstore(0x84, 0x20) // offset of the error string
+                mstore(0xc3, '\x13ETH_TRANSFER_FAILED') // error string's length and bytes
+                revert(0x80, 0x64) // revert with (offset, size)
+            }
         }
-        if (!success) revert ETHtransferFailed();
     }
 
     function _safeTransfer(
@@ -23,8 +24,6 @@ library SafeTransferLib {
         address to,
         uint256 amount
     ) internal {
-        bool success;
-
         assembly {
             // we'll write our calldata to this slot below, but restore it later
             let memPointer := mload(0x40)
@@ -33,7 +32,7 @@ library SafeTransferLib {
             mstore(4, to) // append the 'to' argument
             mstore(36, amount) // append the 'amount' argument
 
-            success := and(
+            let success := and(
                 // set success to whether the call reverted, if not we check it either
                 // returned exactly 1 (can't just be non-zero data), or had no return data
                 or(and(eq(mload(0), 1), gt(returndatasize(), 31)), iszero(returndatasize())),
@@ -42,10 +41,16 @@ library SafeTransferLib {
                 // surrounding and() because and() evaluates its arguments from right to left
                 call(gas(), token, 0, 0, 68, 0, 32)
             )
+            
+            if iszero(success) {
+                mstore(0x64, 0x08c379a0) // function selector of the error method
+                mstore(0x84, 0x20) // offset of the error string
+                mstore(0xc3, "\x0fTRANSFER_FAILED") // error string's length and bytes
+                revert(0x80, 0x64) // revert with (offset, size)
+            }
 
             mstore(0x60, 0) // restore the zero slot to zero
             mstore(0x40, memPointer) // restore the memPointer
         }
-        if (!success) revert TransferFailed();
     }
 }
