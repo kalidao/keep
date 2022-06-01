c3a9683b860bae9e7a58e50eb74fe9f191748951
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-06-01 20:54:14 +0530

    Update ClubNFT.sol
    license

--- src/ClubNFT.sol
+++ src/ClubNFT.sol
@@ -15,7 +15,7 @@ interface ERC721TokenReceiver {
 
 /// @notice Modern, minimalist, and gas efficient ERC-721 implementation designed for Kali ClubSig
 /// @author Modified from Solmate (https://github.com/Rari-Capital/solmate/blob/main/src/tokens/ERC721.sol)
-/// License-Identifier: AGPL-3.0-only
+/// License-Identifier: MIT
 abstract contract ClubNFT {
     /// -----------------------------------------------------------------------
     /// Events
