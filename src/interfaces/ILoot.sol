16c754267aa38666d1574b5965e6565372b7a5f7
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-06-03 00:08:51 +0530

    Update and rename IClubLoot.sol to ILoot.sol

--- src/interfaces/ILoot.sol
+++ src/interfaces/ILoot.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity >=0.8.4;
 
-/// @notice Minimal ERC-20 interface with Kali ClubSig extensions
+/// @notice 'Loot' ERC-20 interface for Kali ClubSig
 /// @dev https://eips.ethereum.org/EIPS/eip-20
-interface IClubLoot {
+interface ILoot {
     function balanceOf(address account) external view returns (uint256);
 
     function totalSupply() external view returns (uint256);
