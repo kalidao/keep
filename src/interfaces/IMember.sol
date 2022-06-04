6e49c9c35cbf71b989d0e0e4d3c5b755c615c804
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-06-04 20:08:27 +0530

    Update IMember.sol
    nit

--- src/interfaces/IMember.sol
+++ src/interfaces/IMember.sol
@@ -4,6 +4,7 @@ pragma solidity >=0.8.4;
 /// @notice Kali ClubSig membership interface
 interface IMember {
     struct Member {
+        bool mint;
         address signer;
         uint256 id;
         uint256 loot;
