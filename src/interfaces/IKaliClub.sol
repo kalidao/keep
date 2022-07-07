de22a19ee20cbbfbbfdc4a7f132346fc9fd7d01d
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-07-07 11:59:30 -0400

    Create IKaliClub.sol

---
+++ src/interfaces/IKaliClub.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity >=0.8.4;
+
+/// @notice Kali Club interface
+interface IKaliClub {
+    function balanceOf(address account, uint256 id) external view returns (uint256);
+
+    function mint(
+        address to,
+        uint256 id,
+        uint256 amount,
+        bytes calldata data
+    ) external payable;
+
+    function burn(
+        address from, 
+        uint256 id, 
+        uint256 amount
+    ) external payable;
+}
