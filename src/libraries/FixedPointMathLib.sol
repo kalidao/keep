2966acf43ece5f083456ceeda26a035529d4ab50
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-07-07 11:39:39 -0400

    Update FixedPointMathLib.sol
    cleanup

--- src/libraries/FixedPointMathLib.sol
+++ src/libraries/FixedPointMathLib.sol
@@ -3,7 +3,6 @@ pragma solidity >=0.8.4;
 
 /// @notice Arithmetic library with operations for fixed-point numbers
 /// @author Modified from Solmate (https://github.com/Rari-Capital/solmate/blob/main/src/utils/FixedPointMathLib.sol)
-/// License-Identifier: AGPL-3.0-only
 library FixedPointMathLib {
     function _mulDivDown(
         uint256 x,
@@ -13,10 +12,12 @@ library FixedPointMathLib {
         assembly {
             // store x * y in z for now
             z := mul(x, y)
+
             // equivalent to require(denominator != 0 && (x == 0 || (x * y) / x == y))
             if iszero(and(iszero(iszero(denominator)), or(iszero(x), eq(div(z, x), y)))) {
                 revert(0, 0)
             }
+
             // divide z by the denominator
             z := div(z, denominator)
         }
