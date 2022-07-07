c4986639cdb6578cf5d85774272af74ac8f63720
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-07-07 11:45:47 -0400

    Update SafeTransferLib.sol
    cleanup

--- src/libraries/SafeTransferLib.sol
+++ src/libraries/SafeTransferLib.sol
@@ -1,24 +1,13 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity >=0.8.4;
 
-/// @notice Safe ETH and ERC-20 transfer library that gracefully handles missing return values
+/// @notice Safe ERC-20 transfer library that gracefully handles missing return values
 /// @author Modified from Solmate (https://github.com/Rari-Capital/solmate/blob/main/src/utils/SafeTransferLib.sol)
-/// License-Identifier: MIT
+/// @dev Caution! This library won't check that a token has code, responsibility is delegated to the caller
 library SafeTransferLib {
-    function _safeTransferETH(address to, uint256 amount) internal {
-        assembly {
-            // transfer the ETH and check if it succeeded or not
-            if iszero(call(gas(), to, amount, 0, 0, 0, 0)) {
-                mstore(0x64, 0x08c379a0) // function selector of the error method, offseted
-                mstore(0x84, 0x20) // offset of the error string
-                mstore(0xc3, '\x13ETH_TRANSFER_FAILED') // error string's length and bytes
-                revert(0x80, 0x64) // revert with (offset, size)
-            }
-        }
-    }
-
-    function _safeTransfer(
+    function _safeTransferFrom(
         address token,
+        address from,
         address to,
         uint256 amount
     ) internal {
@@ -27,25 +16,27 @@ library SafeTransferLib {
             let memPointer := mload(0x40)
 
             // write the abi-encoded calldata into memory, beginning with the function selector
-            mstore(0, 0xa9059cbb00000000000000000000000000000000000000000000000000000000)
-            mstore(4, to) // append the 'to' argument
-            mstore(36, amount) // append the 'amount' argument
+            mstore(0x00, 0x23b872dd)
+            mstore(0x20, from) // append the "from" argument
+            mstore(0x40, to) // append the "to" argument
+            mstore(0x60, amount) // append the "amount" argument
 
             if iszero(
                 and(
                     // set success to whether the call reverted, if not we check it either
                     // returned exactly 1 (can't just be non-zero data), or had no return data
-                    or(and(eq(mload(0), 1), gt(returndatasize(), 31)), iszero(returndatasize())),
-                    // we use 68 because that's the total length of our calldata (4 + 32 * 2)
+                    or(eq(mload(0x00), 1), iszero(returndatasize())),
+                    // we use 0x64 because that's the total length of our calldata (0x04 + 0x20 * 3)
                     // - counterintuitively, this call() must be positioned after the or() in the
                     // surrounding and() because and() evaluates its arguments from right to left
-                    call(gas(), token, 0, 0, 68, 0, 32)
+                    call(gas(), token, 0, 0x1c, 0x64, 0x00, 0x20)
                 )
             ) {
-                mstore(0x64, 0x08c379a0) // function selector of the error method
-                mstore(0x84, 0x20) // offset of the error string
-                mstore(0xc3, '\x0fTRANSFER_FAILED') // error string's length and bytes
-                revert(0x80, 0x64) // revert with (offset, size)
+                mstore(0x00, hex"08c379a0") // function selector of the error method
+                mstore(0x04, 0x20) // offset of the error string
+                mstore(0x24, 20) // length of the error string
+                mstore(0x44, "TRANSFER_FROM_FAILED") // the error string
+                revert(0x00, 0x64) // revert with (offset, size)
             }
 
             mstore(0x60, 0) // restore the zero slot to zero
