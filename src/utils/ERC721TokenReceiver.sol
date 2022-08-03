136448dcc35fd91a52591b5886c53f5b8e9b6546
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-08-02 20:01:59 -0400

    Update ERC721TokenReceiver.sol
    💈

--- src/utils/ERC721TokenReceiver.sol
+++ src/utils/ERC721TokenReceiver.sol
@@ -2,7 +2,7 @@
 pragma solidity >=0.8.4;
 
 /// @notice A generic interface for a contract which properly accepts ERC721 tokens
-/// @author Modified from Solmate (https://github.com/Rari-Capital/solmate/blob/main/src/tokens/ERC721.sol)
+/// @author Modified from Solmate (https://github.com/transmissions11/solmate/blob/main/src/tokens/ERC721.sol)
 abstract contract ERC721TokenReceiver {
     function onERC721Received(
         address,
