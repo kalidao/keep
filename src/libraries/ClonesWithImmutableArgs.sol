d5afa653f9ac3705c7bf63897a0ff192de4976c0
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-08-24 12:09:09 +0900

    Update ClonesWithImmutableArgs.sol
    wminshew black magick

--- src/libraries/ClonesWithImmutableArgs.sol
+++ src/libraries/ClonesWithImmutableArgs.sol
@@ -5,13 +5,18 @@ pragma solidity >=0.8.4;
 /// @author Modified from wighawag, zefram.eth, Saw-mon & Natalie, will@0xsplits.xyz
 /// (https://github.com/wighawag/clones-with-immutable-args/blob/master/src/ClonesWithImmutableArgs.sol)
 library ClonesWithImmutableArgs {
-    error CREATE2_FAILED();
+    error CreateFail();
 
-    uint256 internal constant FREE_MEMORY_POINTER_SLOT = 0x40;
-    uint256 internal constant BOOTSTRAP_LENGTH = 0x3f;
-    uint256 internal constant ONE_WORD = 0x20;
+    uint256 private constant FREE_MEMORY_POINTER_SLOT = 0x40;
+    uint256 private constant BOOTSTRAP_LENGTH = 0x6f;
+    uint256 private constant RUNTIME_BASE = 0x65; // BOOTSTRAP_LENGTH - 10 bytes
+    uint256 private constant ONE_WORD = 0x20;
+    // = keccak256("ReceiveETH(uint256)")
+    uint256 private constant RECEIVE_EVENT_SIG =
+        0x9e4ac34f21c619cefc926c8bd93b54bf5a39c7ab2127a895af1cc0691d7e3dff;
 
     /// @notice Creates a clone proxy of the implementation contract with immutable args
+    /// @dev data cannot exceed 65535 bytes, since 2 bytes are used to store the data length
     /// @param implementation The implementation contract to clone
     /// @param data Encoded immutable args
     /// @return ptr The ptr to the clone's bytecode
@@ -21,32 +26,44 @@ library ClonesWithImmutableArgs {
         pure
         returns (uint256 ptr, uint256 creationSize)
     {
+        // unrealistic for memory ptr or data length to exceed 256 bits
         assembly {
-            let extraLength := add(mload(data), 2)
+            let extraLength := add(mload(data), 2) // +2 bytes for telling how much data there is appended to the call
             creationSize := add(extraLength, BOOTSTRAP_LENGTH)
             let runSize := sub(creationSize, 0x0a)
 
+            // free memory pointer
             ptr := mload(FREE_MEMORY_POINTER_SLOT)
-                
+
             mstore(
                 ptr,
                 or(
-                    hex"610000_3d_81_600a_3d_39_f3_36_3d_3d_37_3d_3d_3d_3d_610000_80_6035_36_39_36_01_3d_73",
-                    or(
-                        shl(0xe8, runSize),
-                        shl(0x58, extraLength)
-                    )
+                    hex"6100003d81600a3d39f336602f57343d527f", // 18 bytes
+                    shl(0xe8, runSize)
                 )
-            )
-                
-            mstore(
-                add(ptr, 0x1e),
-                shl(0x60, implementation)
             )
 
             mstore(
-                add(ptr, 0x32),
-                hex"5a_f4_3d_3d_93_80_3e_6033_57_fd_5b_f3"
+                   add(ptr, 0x12), // 0x0 + 0x12
+                RECEIVE_EVENT_SIG // 32 bytes
+            )
+
+            mstore(
+                   add(ptr, 0x32), // 0x12 + 0x20
+                or(
+                    hex"60203da13d3df35b363d3d373d3d3d3d610000806000363936013d73", // 28 bytes
+                    or(shl(0x68, extraLength), shl(0x50, RUNTIME_BASE))
+                )
+            )
+
+            mstore(
+                   add(ptr, 0x4e), // 0x32 + 0x1c
+                shl(0x60, implementation) // 20 bytes
+            )
+
+            mstore(
+                   add(ptr, 0x62), // 0x4e + 0x14
+                hex"5af43d3d93803e606357fd5bf3" // 13 bytes
             )
 
             let counter := mload(data)
@@ -54,26 +71,23 @@ library ClonesWithImmutableArgs {
             let dataPtr := add(data, ONE_WORD)
 
             for {} true {} {
-                if lt(counter, ONE_WORD) {
-                    break
-                }
+                if lt(counter, ONE_WORD) { break }
 
                 mstore(copyPtr, mload(dataPtr))
 
                 copyPtr := add(copyPtr, ONE_WORD)
                 dataPtr := add(dataPtr, ONE_WORD)
+
                 counter := sub(counter, ONE_WORD)
             }
-                    
-            let mask := shl(
-                mul(0x8, sub(ONE_WORD, counter)), 
-                not(0)
-            )
+
+            let mask := shl(mul(0x8, sub(ONE_WORD, counter)), not(0))
 
             mstore(copyPtr, and(mload(dataPtr), mask))
             copyPtr := add(copyPtr, counter)
             mstore(copyPtr, shl(0xf0, extraLength))
 
+            // update free memory pointer
             mstore(FREE_MEMORY_POINTER_SLOT, add(ptr, creationSize))
         }
     }
