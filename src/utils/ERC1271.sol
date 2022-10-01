b5297dc1cee28cff70f459dd65e580f0d5b31254
z0r0z <zrzr8963@gmail.com>
2022-10-01 15:55:08 +0900

    🥢Comments

--- src/utils/ERC1271.sol
+++ src/utils/ERC1271.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-/// @notice ERC-1271 interface.
-/// @dev https://eips.ethereum.org/EIPS/eip-1271
+/// @notice ERC-1271 interface (https://eips.ethereum.org/EIPS/eip-1271).
 abstract contract ERC1271 {
     function isValidSignature(bytes32 hash, bytes calldata signature)
         external
