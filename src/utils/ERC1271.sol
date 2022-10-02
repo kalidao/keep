16494f27c76cd27b7bf994f20f61a3f9ffe396cb
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-10-02 14:05:20 +0530

    ⚡ Refactor (#98)
    * 📷 Snap
    
    * ⚖️ MIT License
    
    * 🥢Comments
    
    * 🥢Comment nits and expose custom error
    
    * ⚡Cleanups and optimizations
    
    * 🥢Comment

--- src/utils/ERC1271.sol
+++ src/utils/ERC1271.sol
@@ -1,11 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-/// @notice ERC-1271 interface.
-/// @dev https://eips.ethereum.org/EIPS/eip-1271
+/// @notice ERC-1271 interface (https://eips.ethereum.org/EIPS/eip-1271).
 abstract contract ERC1271 {
     function isValidSignature(bytes32 hash, bytes calldata signature)
-        external
+        public
         view
         virtual
         returns (bytes4);
