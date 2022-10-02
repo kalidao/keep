60d494b3a222b4720efae899ea2d52281b2de1d0
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-10-02 18:13:08 +0900

    Update ERC1271.sol
    🥢 Comment

--- src/utils/ERC1271.sol
+++ src/utils/ERC1271.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-/// @notice ERC-1271 interface (https://eips.ethereum.org/EIPS/eip-1271).
+/// @notice ERC1271 interface (https://eips.ethereum.org/EIPS/eip-1271).
 abstract contract ERC1271 {
     function isValidSignature(bytes32 hash, bytes calldata signature)
         public
