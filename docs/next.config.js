6035b4d61ac1eb25cfdb694b1a15c4bde8b5810e
shiv <shivanshityagi@icloud.com>
2022-10-11 08:40:49 +0000

    experimental

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,7 +1,12 @@
+const config = {
+    experimental: {
+        externalDir: true,
+    },
+}
 const withNextra = require('nextra')({
     theme: 'nextra-theme-docs',
     themeConfig: './theme.config.js',
     // optional: add `unstable_staticImage: true` to enable Nextra's auto image import
 })
 
-module.exports = withNextra()
+module.exports = withNextra(config)
