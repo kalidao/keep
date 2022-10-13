22897448564b3c2f51ab28b4e1fd6828b3f954b6
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-10-13 11:32:06 +0900

    Create KaliExtension.sol
    📁  Organize

---
+++ src/dao/utils/KaliExtension.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+/// @notice Interface for Kali DAO extensions.
+abstract contract KaliExtension {
+    function setExtension(bytes calldata extensionData) external virtual;
+}
