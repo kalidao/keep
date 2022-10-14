2a87266d0dacd440c6b888019ca4c54e3804cf5a
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-10-14 18:00:22 +0900

    Update MockSmartWallet.sol
    🧪

--- test/mocks/MockSmartWallet.sol
+++ test/mocks/MockSmartWallet.sol
@@ -2,9 +2,8 @@
 pragma solidity ^0.8.4;
 
 import {MockERC1271Wallet} from "@solbase/test/utils/mocks/MockERC1271Wallet.sol";
-import {ERC1155TokenReceiver} from "../../src/Keep.sol";
 
-contract MockSmartWallet is MockERC1271Wallet, ERC1155TokenReceiver {
+contract MockSmartWallet is MockERC1271Wallet {
     constructor(address signer_) MockERC1271Wallet(signer_) {}
 }
 
