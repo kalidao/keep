885c5da1137854e8bf55045b932b6e50149bef70
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-10-14 18:35:15 +0900

    ✨ User URI (#115)
    * Update URIRemoteFetcher.sol
    
    🔧 user uri
    
    * Update URIRemoteFetcher.sol
    
    ✨ User id metadata concept

--- src/extensions/URIRemoteFetcher.sol
+++ src/extensions/URIRemoteFetcher.sol
@@ -9,18 +9,33 @@ contract URIRemoteFetcher is Owned {
     /// Events
     /// -----------------------------------------------------------------------
 
-    event AlphaURISet(address indexed user, string alphaURI);
+    event AlphaURISet(address indexed operator, string alphaURI);
 
     event BetaURISet(
-        address indexed user,
+        address indexed operator,
         address indexed origin,
         string betaURI
     );
 
     event URISet(
-        address indexed user,
+        address indexed operator,
         address indexed origin,
         uint256 indexed id,
+        string uri
+    );
+    
+    event UserURISet(
+        address indexed operator,
+        address indexed origin,
+        address indexed user,
+        string uri
+    );
+    
+    event UserIdURISet(
+        address indexed operator,
+        address indexed origin,
+        address indexed user,
+        uint256 id,
         string uri
     );
 
@@ -33,6 +48,10 @@ contract URIRemoteFetcher is Owned {
     mapping(address => string) public betaURI;
 
     mapping(address => mapping(uint256 => string)) public uris;
+    
+    mapping(address => mapping(address => string)) public userUris;
+    
+    mapping(address => mapping(address => mapping(uint256 => string))) public userIdUris;
 
     /// -----------------------------------------------------------------------
     /// Constructor
@@ -89,5 +108,26 @@ contract URIRemoteFetcher is Owned {
         uris[origin][id] = uri;
 
         emit URISet(msg.sender, origin, id, uri);
+    }
+    
+    function setUserURI(
+        address origin,
+        address user,
+        string calldata uri
+    ) public payable virtual onlyOwner {
+        userUris[origin][user] = uri;
+
+        emit UserURISet(msg.sender, origin, user, uri);
+    }
+    
+    function setUserIdURI(
+        address origin,
+        address user,
+        uint256 id,
+        string calldata uri
+    ) public payable virtual onlyOwner {
+        userIdUris[origin][user][id] = uri;
+
+        emit UserIdURISet(msg.sender, origin, user, id, uri);
     }
 }
