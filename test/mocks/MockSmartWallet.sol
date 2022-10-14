c5b08e238300df4b1194b761049c10c05046837c
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-10-14 18:03:39 +0900

    Delete MockSmartWallet.sol

--- test/mocks/MockSmartWallet.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
-
-import {MockERC1271Wallet} from "@solbase/test/utils/mocks/MockERC1271Wallet.sol";
-
-contract MockSmartWallet is MockERC1271Wallet {
-    constructor(address signer_) MockERC1271Wallet(signer_) {}
-}
-
