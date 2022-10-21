d3f42182cf4896e970858868059b354c57210a2a
z0r0z <92001561+z0r0z@users.noreply.github.com>
2022-10-21 18:02:42 +0900

    🧹 Clean and expand tests
    🧹 Clean and expand tests

--- test/Keep.t.sol
+++ test/Keep.t.sol
@@ -44,24 +44,26 @@ contract KeepTest is ERC1155TokenReceiver, Test {
 
     uint256 internal SIGNER_KEY;
 
+    address[] signers;
+
     /// @dev Users.
 
-    uint256 internal immutable alicesPk =
+    uint256 internal constant alicesPk =
         0x60b919c82f0b4791a5b7c6a7275970ace1748759ebdaa4076d7eeed9dbcff3c3;
-    address public immutable alice = 0x503408564C50b43208529faEf9bdf9794c015d52;
+    address public constant alice = 0x503408564C50b43208529faEf9bdf9794c015d52;
 
-    uint256 internal immutable bobsPk =
+    uint256 internal constant bobsPk =
         0xf8f8a2f43c8376ccb0871305060d7b27b0554d2cc72bccf41b2705608452f315;
-    address public immutable bob = 0x001d3F1ef827552Ae1114027BD3ECF1f086bA0F9;
+    address public constant bob = 0x001d3F1ef827552Ae1114027BD3ECF1f086bA0F9;
 
-    uint256 internal immutable charliesPk =
+    uint256 internal constant charliesPk =
         0xb9dee2522aae4d21136ba441f976950520adf9479a3c0bda0a88ffc81495ded3;
-    address public immutable charlie =
+    address public constant charlie =
         0xccc4A5CeAe4D88Caf822B355C02F9769Fb6fd4fd;
 
-    uint256 internal immutable nullPk =
+    uint256 internal constant nullPk =
         0x8b2ed20f3cc3dd482830910365cfa157e7568b9c3fa53d9edd3febd61086b9be;
-    address public immutable nully = 0x0ACDf2aC839B7ff4cd5F16e884B2153E902253f2;
+    address public constant nully = 0x0ACDf2aC839B7ff4cd5F16e884B2153E902253f2;
 
     /// @dev Helpers.
 
@@ -194,17 +196,21 @@ contract KeepTest is ERC1155TokenReceiver, Test {
         // Create the factory.
         factory = new KeepFactory(keep);
 
-        // Create the Signer[].
-        address[] memory signers = new address[](2);
-        signers[0] = alice > bob ? bob : alice;
-        signers[1] = alice > bob ? alice : bob;
+        // Create the Signer[] for setup.
+        address[] memory setupSigners = new address[](2);
+        setupSigners[0] = alice > bob ? bob : alice;
+        setupSigners[1] = alice > bob ? alice : bob;
+
+        // Store the signers for later.
+        signers.push(alice > bob ? bob : alice);
+        signers.push(alice > bob ? alice : bob);
 
         // Initialize Keep from factory.
         // The factory is fully tested in KeepFactory.t.sol.
         keepAddr = factory.determineKeep(name);
         keep = Keep(keepAddr);
 
-        factory.deployKeep(name, calls, signers, 2);
+        factory.deployKeep(name, calls, setupSigners, 2);
 
         // Store signer ID key.
         SIGNER_KEY = uint32(keep.execute.selector);
@@ -308,10 +314,6 @@ contract KeepTest is ERC1155TokenReceiver, Test {
     function testCannotRepeatKeepSetup() public payable {
         keepRepeat = new Keep(Keep(address(uriFetcher)));
 
-        address[] memory signers = new address[](2);
-        signers[0] = alice > bob ? bob : alice;
-        signers[1] = alice > bob ? alice : bob;
-
         keepAddrRepeat = factory.determineKeep(name2);
         keepRepeat = Keep(keepAddrRepeat);
         factory.deployKeep(name2, calls, signers, 2);
@@ -321,30 +323,22 @@ contract KeepTest is ERC1155TokenReceiver, Test {
     }
 
     function testCannotSetupWithZeroQuorum() public payable {
-        address[] memory signers = new address[](2);
-        signers[0] = alice > bob ? bob : alice;
-        signers[1] = alice > bob ? alice : bob;
-
         vm.expectRevert(bytes(""));
         factory.deployKeep(name2, calls, signers, 0);
     }
 
     function testCannotSetupWithExcessiveQuorum() public payable {
-        address[] memory signers = new address[](2);
-        signers[0] = alice > bob ? bob : alice;
-        signers[1] = alice > bob ? alice : bob;
-
         vm.expectRevert(bytes4(keccak256("QuorumOverSupply()")));
         factory.deployKeep(name2, calls, signers, 3);
     }
 
     function testCannotSetupWithOutOfOrderSigners() public payable {
-        address[] memory signers = new address[](2);
-        signers[0] = alice > bob ? alice : bob;
-        signers[1] = alice > bob ? bob : alice;
+        address[] memory outOfOrderSigners = new address[](2);
+        outOfOrderSigners[0] = alice > bob ? alice : bob;
+        outOfOrderSigners[1] = alice > bob ? bob : alice;
 
         vm.expectRevert(bytes4(keccak256("InvalidSig()")));
-        factory.deployKeep(name2, calls, signers, 2);
+        factory.deployKeep(name2, calls, outOfOrderSigners, 2);
     }
 
     /// -----------------------------------------------------------------------
@@ -436,7 +430,6 @@ contract KeepTest is ERC1155TokenReceiver, Test {
     /// @dev Check call execution.
 
     function testExecuteCallWithRole() public payable {
-        address aliceAddr = alice;
         uint256 nonceInit = keep.nonce();
 
         // Mint executor role.
@@ -447,11 +440,11 @@ contract KeepTest is ERC1155TokenReceiver, Test {
         // Mock execution.
         startHoax(address(alice), address(alice), type(uint256).max);
 
-        bytes memory data = "";
+        bytes memory data;
 
         assembly {
             mstore(add(data, 0x20), shl(0xE0, 0xa9059cbb)) // `transfer(address,uint256)`.
-            mstore(add(data, 0x24), aliceAddr)
+            mstore(add(data, 0x24), alice)
             mstore(add(data, 0x44), 100)
             mstore(data, 0x44)
             // Update free memory pointer.
@@ -474,12 +467,11 @@ contract KeepTest is ERC1155TokenReceiver, Test {
     }
 
     function testExecuteCallWithSignatures() public payable {
-        address aliceAddress = alice;
-        bytes memory tx_data = "";
+        bytes memory tx_data;
 
         assembly {
             mstore(add(tx_data, 0x20), shl(0xE0, 0xa9059cbb)) // `transfer(address,uint256)`.
-            mstore(add(tx_data, 0x24), aliceAddress)
+            mstore(add(tx_data, 0x24), alice)
             mstore(add(tx_data, 0x44), 100)
             mstore(tx_data, 0x44)
             // Update free memory pointer.
@@ -488,10 +480,7 @@ contract KeepTest is ERC1155TokenReceiver, Test {
 
         Signature[] memory sigs = new Signature[](2);
 
-        Signature memory aliceSig;
-        Signature memory bobSig;
-
-        aliceSig = signExecution(
+        Signature memory aliceSig = signExecution(
             alice,
             alicesPk,
             Operation.call,
@@ -500,7 +489,7 @@ contract KeepTest is ERC1155TokenReceiver, Test {
             tx_data
         );
 
-        bobSig = signExecution(
+        Signature memory bobSig = signExecution(
             bob,
             bobsPk,
             Operation.call,
@@ -517,12 +506,11 @@ contract KeepTest is ERC1155TokenReceiver, Test {
     }
 
     function testExecuteDelegateCallWithSignatures() public payable {
-        address aliceAddress = alice;
-        bytes memory tx_data = "";
+        bytes memory tx_data;
 
         assembly {
             mstore(add(tx_data, 0x20), shl(0xE0, 0x70a08231)) // `balanceOf(address)`.
-            mstore(add(tx_data, 0x24), aliceAddress)
+            mstore(add(tx_data, 0x24), alice)
             mstore(tx_data, 0x24)
             // Update free memory pointer.
             mstore(0x40, add(tx_data, 0x60))
@@ -589,12 +577,11 @@ contract KeepTest is ERC1155TokenReceiver, Test {
     }
 
     function testExecuteCallWithContractSignatures() public payable {
-        address aliceAddress = alice;
-        bytes memory tx_data = "";
+        bytes memory tx_data;
 
         assembly {
             mstore(add(tx_data, 0x20), shl(0xE0, 0xa9059cbb)) // `transfer(address,uint256)`.
-            mstore(add(tx_data, 0x24), aliceAddress)
+            mstore(add(tx_data, 0x24), alice)
             mstore(add(tx_data, 0x44), 100)
             mstore(tx_data, 0x44)
             // Update free memory pointer.
@@ -634,12 +621,11 @@ contract KeepTest is ERC1155TokenReceiver, Test {
     /// @dev Check execution errors.
 
     function testCannotExecuteWithImproperSignatures() public payable {
-        address aliceAddress = alice;
-        bytes memory tx_data = "";
+        bytes memory tx_data;
 
         assembly {
             mstore(add(tx_data, 0x20), shl(0xE0, 0xa9059cbb)) // `transfer(address,uint256)`.
-            mstore(add(tx_data, 0x24), aliceAddress)
+            mstore(add(tx_data, 0x24), alice)
             mstore(add(tx_data, 0x44), 100)
             mstore(tx_data, 0x44)
             // Update free memory pointer.
@@ -678,12 +664,11 @@ contract KeepTest is ERC1155TokenReceiver, Test {
     }
 
     function testCannotExecuteWithSignaturesOutOfOrder() public payable {
-        address aliceAddress = alice;
-        bytes memory tx_data = "";
+        bytes memory tx_data;
 
         assembly {
             mstore(add(tx_data, 0x20), shl(0xE0, 0xa9059cbb)) // `transfer(address,uint256)`.
-            mstore(add(tx_data, 0x24), aliceAddress)
+            mstore(add(tx_data, 0x24), alice)
             mstore(add(tx_data, 0x44), 100)
             mstore(tx_data, 0x44)
             // Update free memory pointer.
@@ -722,12 +707,11 @@ contract KeepTest is ERC1155TokenReceiver, Test {
     }
 
     function testCannotExecuteWithSignaturesRepeated() public payable {
-        address aliceAddress = alice;
-        bytes memory tx_data = "";
+        bytes memory tx_data;
 
         assembly {
             mstore(add(tx_data, 0x20), shl(0xE0, 0xa9059cbb)) // `transfer(address,uint256)`.
-            mstore(add(tx_data, 0x24), aliceAddress)
+            mstore(add(tx_data, 0x24), alice)
             mstore(add(tx_data, 0x44), 100)
             mstore(tx_data, 0x44)
             // Update free memory pointer.
@@ -756,12 +740,11 @@ contract KeepTest is ERC1155TokenReceiver, Test {
     }
 
     function testCannotExecuteWithNullSignatures() public payable {
-        address aliceAddress = alice;
-        bytes memory tx_data = "";
+        bytes memory tx_data;
 
         assembly {
             mstore(add(tx_data, 0x20), shl(0xE0, 0xa9059cbb)) // `transfer(address,uint256)`.
-            mstore(add(tx_data, 0x24), aliceAddress)
+            mstore(add(tx_data, 0x24), alice)
             mstore(add(tx_data, 0x44), 100)
             mstore(tx_data, 0x44)
             // Update free memory pointer.
@@ -1372,18 +1355,16 @@ contract KeepTest is ERC1155TokenReceiver, Test {
         vm.assume(userB.code.length == 0);
         vm.assume(id != SIGNER_KEY);
 
-        vm.prank(address(keep));
-        keep.setTransferability(id, true);
-        vm.stopPrank();
-
-        assertTrue(keep.transferable(id));
-
         startHoax(address(keep), address(keep), type(uint256).max);
         keep.mint(userA, id, amount, "");
+        keep.setTransferability(id, true);
         keep.setPermission(id, true);
         vm.stopPrank();
 
-        startHoax(userA, userA, type(uint256).max);
+        assertTrue(keep.transferable(id));
+        assertTrue(keep.permissioned(id));
+
+        vm.prank(userA);
         vm.expectRevert(bytes4(keccak256("NotPermitted()")));
         keep.safeTransferFrom(userA, userB, id, amount, "");
         vm.stopPrank();
