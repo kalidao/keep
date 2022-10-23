39aa7498463a4d2acb44a81c86c6b43257c13e32
z0r0z <zrzr8963@gmail.com>
2022-10-23 20:38:21 +0900

    🔪 Trim folder

--- src/dao/utils/KaliExtension.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
-
-/// @notice Contract helper for Kali DAO extensions.
-abstract contract KaliExtension {
-    function setExtension(bytes calldata extensionData) public virtual;
-}
