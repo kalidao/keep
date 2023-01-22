ace60ffc8cf6edb477991c6d39a5e065de38f1d3
z0r0z <92001561+z0r0z@users.noreply.github.com>
2023-01-23 01:15:22 +0530

    ✍️ 💸  payable helper
    ✍️ 💸  payable helper

--- src/extensions/dao/utils/KaliExtension.sol
+++ src/extensions/dao/utils/KaliExtension.sol
@@ -3,5 +3,5 @@ pragma solidity ^0.8.4;
 
 /// @notice Contract helper for Kali DAO extensions.
 abstract contract KaliExtension {
-    function setExtension(bytes calldata extensionData) public virtual;
+    function setExtension(bytes calldata extensionData) public payable virtual;
 }
