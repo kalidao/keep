865be3e85b0a821cf6b71b501afb116073686b3c
ross <92001561+z0r0z@users.noreply.github.com>
2023-08-26 04:44:11 +0530

    ``

--- src/KeepFactory.sol
+++ src/KeepFactory.sol
@@ -23,23 +23,23 @@ contract KeepFactory is Multicallable, Owned(tx.origin) {
     /// Immutables
     /// -----------------------------------------------------------------------
 
-    Keep internal immutable keepTemplate;
+    Keep public immutable keepTemplate;
+
+    /// @dev The ERC-1967 storage slot for the implementation in the proxy.
+    /// `uint256(keccak256("eip1967.proxy.implementation")) - 1`.
+    uint256 internal constant _IMPLEMENTATION_SLOT = 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
 
     /// -----------------------------------------------------------------------
     /// Constructor
     /// -----------------------------------------------------------------------
 
-    constructor(Keep _keepTemplate) payable {
-        keepTemplate = _keepTemplate;
+    constructor() payable {
+        keepTemplate = new Keep(Keep(address(new Validator())));
     }
 
     /// -----------------------------------------------------------------------
     /// Deployment Logic
     /// -----------------------------------------------------------------------
-
-    function determineKeep(bytes32 name) public view virtual returns (address) {
-        return predictDeterministicAddress(name);
-    }
 
     function deployKeep(
         bytes32 name, // create2 salt.
@@ -47,39 +47,32 @@ contract KeepFactory is Multicallable, Owned(tx.origin) {
         address[] calldata signers,
         uint256 threshold
     ) public payable virtual returns (Keep keep) {
-        keep = cloneDeterministic(name);
-
-        keep.initialize{value: msg.value}(calls, signers, threshold);
-
-        emit Deployed(keep, threshold);
-    }
-
-    /// -----------------------------------------------------------------------
-    /// Clone Operations
-    /// -----------------------------------------------------------------------
-
-    /// @dev Deploys a deterministic clone of `keepTemplate`,
-    /// using immutable argument `name` also as `salt`.
-    function cloneDeterministic(bytes32 salt) internal returns (Keep instance) {
         address implementation = address(keepTemplate);
-        bytes memory data = abi.encodePacked(salt);
 
         assembly {
+            let data := mload(0x40) // Get the current free memory pointer.
+            
+            // Store the salt (data) at the start of the memory segment.
+            mstore(data, name)
+
             // Compute the boundaries of the data and cache the memory slots around it.
             let mBefore3 := mload(sub(data, 0x60))
             let mBefore2 := mload(sub(data, 0x40))
             let mBefore1 := mload(sub(data, 0x20))
-            let dataLength := mload(data)
-            let dataEnd := add(add(data, 0x20), dataLength)
-            let mAfter1 := mload(dataEnd)
-
+            
             // +2 bytes for telling how much data there is appended to the call.
-            let extraLength := add(dataLength, 2)
+            let extraLength := 0x22 // 0x20 (salt length) + 2
+
+            let dataEnd := add(data, 0x20)
+
+            let mAfter1 := mload(dataEnd)
 
             // Write the bytecode before the data.
             mstore(data, 0x5af43d3d93803e606057fd5bf3)
+            
             // Write the address of the implementation.
             mstore(sub(data, 0x0d), implementation)
+            
             // Write the rest of the bytecode.
             mstore(
                 sub(data, 0x21),
@@ -88,11 +81,13 @@ contract KeepFactory is Multicallable, Owned(tx.origin) {
                     0x593da1005b363d3d373d3d3d3d610000806062363936013d73
                 )
             )
+            
             // `keccak256("ReceiveETH(uint256)")`.
             mstore(
                 sub(data, 0x3a),
                 0x9e4ac34f21c619cefc926c8bd93b54bf5a39c7ab2127a895af1cc0691d7e3dff
             )
+            
             mstore(
                 sub(data, 0x5a),
                 or(
@@ -100,58 +95,73 @@ contract KeepFactory is Multicallable, Owned(tx.origin) {
                     0x6100003d81600a3d39f336602c57343d527f
                 )
             )
+            
             mstore(dataEnd, shl(0xf0, extraLength))
 
             // Create the instance.
-            instance := create2(
+            keep := create2(
                 0,
                 sub(data, 0x4c),
                 add(extraLength, 0x6c),
-                salt
+                name
             )
 
             // If `instance` is zero, revert.
-            if iszero(instance) {
+            if iszero(keep) {
                 // Store the function selector of `DeploymentFailed()`.
                 mstore(0x00, 0x30116425)
                 // Revert with (offset, size).
                 revert(0x1c, 0x04)
             }
 
+            // Set the implementation on the new proxy.
+            mstore(0x00, sload(_IMPLEMENTATION_SLOT))  // Load the current value in case of collision (very unlikely).
+            mstore(0x20, implementation)               // New implementation address.
+            // Set the implementation on the new proxy.
+            mstore(0x20, _IMPLEMENTATION_SLOT)
+            sstore(keep, 0x20)
+
             // Restore the overwritten memory surrounding `data`.
             mstore(dataEnd, mAfter1)
-            mstore(data, dataLength)
+            mstore(data, 0x20) // salt length
             mstore(sub(data, 0x20), mBefore1)
             mstore(sub(data, 0x40), mBefore2)
             mstore(sub(data, 0x60), mBefore3)
         }
+
+        keep.initialize{value: msg.value}(calls, signers, threshold);
+
+        emit Deployed(keep, threshold);
     }
 
-    /// @dev Returns the address of the deterministic clone of
-    /// `keepTemplate` using immutable argument `name` also as `salt`.
-    function predictDeterministicAddress(
-        bytes32 salt
-    ) internal view returns (address predicted) {
+    function determineKeep(bytes32 name) public view virtual returns (address keep, bool deployed) {
         address implementation = address(keepTemplate);
         address deployer = address(this);
-        bytes memory data = abi.encodePacked(salt);
 
         assembly {
+            let data := mload(0x40) // Get the current free memory pointer.
+            
+            // Store the salt (data) at the start of the memory segment.
+            mstore(data, name)
+
             // Compute the boundaries of the data and cache the memory slots around it.
             let mBefore3 := mload(sub(data, 0x60))
             let mBefore2 := mload(sub(data, 0x40))
             let mBefore1 := mload(sub(data, 0x20))
-            let dataLength := mload(data)
-            let dataEnd := add(add(data, 0x20), dataLength)
-            let mAfter1 := mload(dataEnd)
-
+            
             // +2 bytes for telling how much data there is appended to the call.
-            let extraLength := add(dataLength, 2)
+            let extraLength := 0x22 // 0x20 (salt length) + 2
+
+            let dataEnd := add(data, 0x20)
+
+            let mAfter1 := mload(dataEnd)
 
             // Write the bytecode before the data.
             mstore(data, 0x5af43d3d93803e606057fd5bf3)
+            
             // Write the address of the implementation.
             mstore(sub(data, 0x0d), implementation)
+            
             // Write the rest of the bytecode.
             mstore(
                 sub(data, 0x21),
@@ -160,11 +170,13 @@ contract KeepFactory is Multicallable, Owned(tx.origin) {
                     0x593da1005b363d3d373d3d3d3d610000806062363936013d73
                 )
             )
+            
             // `keccak256("ReceiveETH(uint256)")`.
             mstore(
                 sub(data, 0x3a),
                 0x9e4ac34f21c619cefc926c8bd93b54bf5a39c7ab2127a895af1cc0691d7e3dff
             )
+            
             mstore(
                 sub(data, 0x5a),
                 or(
@@ -172,20 +184,20 @@ contract KeepFactory is Multicallable, Owned(tx.origin) {
                     0x6100003d81600a3d39f336602c57343d527f
                 )
             )
+            
             mstore(dataEnd, shl(0xf0, extraLength))
 
             // Compute and store the bytecode hash.
             mstore(0x35, keccak256(sub(data, 0x4c), add(extraLength, 0x6c)))
             mstore8(0x00, 0xff) // Write the prefix.
             mstore(0x01, shl(96, deployer))
-            mstore(0x15, salt)
-            predicted := keccak256(0x00, 0x55)
-            // Restore the part of the free memory pointer that has been overwritten.
-            mstore(0x35, 0)
+            mstore(0x15, name)
+            keep := keccak256(0x00, 0x55)
+            deployed := extcodesize(keep)
 
             // Restore the overwritten memory surrounding `data`.
             mstore(dataEnd, mAfter1)
-            mstore(data, dataLength)
+            mstore(data, 0x20) // salt length
             mstore(sub(data, 0x20), mBefore1)
             mstore(sub(data, 0x40), mBefore2)
             mstore(sub(data, 0x60), mBefore3)
@@ -197,7 +209,7 @@ contract KeepFactory is Multicallable, Owned(tx.origin) {
     /// -----------------------------------------------------------------------
 
     function addStake(
-        uint256 unstakeDelaySec
+        uint32 unstakeDelaySec
     ) public payable virtual onlyOwner {
         KeepFactory(keepTemplate.entryPoint()).addStake{value: msg.value}(
             unstakeDelaySec
